09c729c32cfdc8e01301f973295c34fcbe1e4046
CarlosSantos29 <carlos2003bsantos@gmail.com>
2025-09-02 21:10:35 -0300

    atualização de arquivos .py

---
+++ Curso-Em-Video-Python/aulas/estrutura_repertição_break.py
@@ -0,0 +1,24 @@
+contador = 1
+'''
+while contador <= 10:
+    print(contador, '', end='')
+    contador += 1
+print('Acabou.')
+'''
+palavra = 'acabou'
+numero = soma = contador = 0
+print('Teste'+^20)
+while True:
+    numero = int(input('Digite um valor: '))
+    '''contador += 1'''
+    if numero == 999:
+        break
+    soma += numero
+print(f'A soma equvale a {soma}')
+print(f'{palavra:^20}')# centralizado.
+
+'''
+exercicios While com Break
+
+1.
+'''
